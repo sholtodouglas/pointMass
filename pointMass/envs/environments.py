8a1651fca44389e235b9edad9ba93f7005631bdb
SholtoD <sdou4571@uni.sydney.edy.au>
2019-08-03 20:17:29 +1000

    added config start pos

--- pointMass/envs/environments.py
+++ pointMass/envs/environments.py
@@ -19,7 +19,7 @@ class pointMassEnv(gym.Env):
 
 		def __init__(self, render = False):
 			action_dim = 2
-			obs_dim = 5
+			obs_dim = 6
 			high = np.ones([action_dim])
 			self.action_space = gym.spaces.Box(-high, high)
 			high = np.inf * np.ones([obs_dim])
@@ -42,6 +42,14 @@ class pointMassEnv(gym.Env):
 			self._p.resetBasePositionAndOrientation(self.goal, [self.goal_x, self.goal_y,0.4], [0,0,0,1])
 			self._p.changeConstraint(self.goal_cid,[self.goal_x, self.goal_y,0.2], maxForce = 100)
 
+		def initalize_start_pos(self, s_i, v_i):
+			x,y,x_vel,y_vel = s_i[0],s_i[1],v_i[0],v_i[1] 
+			self._p.resetBasePositionAndOrientation(self.mass, [x, y,-0.1], [0,0,0,1])
+			self._p.changeConstraint(self.mass_cid,[x, y,-0.1], maxForce = 100)
+			#self._p.resetBaseVelocity(self.mass,[x_vel, y_vel, 0])
+
+
+
 
 			
 		def calc_state(self):
@@ -49,8 +57,12 @@ class pointMassEnv(gym.Env):
 			# state will be x,y pos, total velocity, x,y goal.
 			current_pos = self._p.getBasePositionAndOrientation(self.mass)[0]
 			x,y = current_pos[0], current_pos[1]
-			velocity = (np.sum(np.array(self._p.getBaseVelocity(self.mass)[0])[0:2])**2)**(1/2)
-			return np.array([x,y,velocity,self.goal_x,self.goal_y])
+			velocity = self._p.getBaseVelocity(self.mass)[0]
+			x_vel, y_vel = velocity[0], velocity[1]
+			#print(x_vel, y_vel)
+			velocity_mag = (np.sum(np.array(self._p.getBaseVelocity(self.mass)[0])[0:2])**2)**(1/2)
+
+			return np.array([x,y,x_vel, y_vel,self.goal_x,self.goal_y])
 
 		def calc_target_distance(self):
 			
@@ -157,6 +169,14 @@ class pointMassEnv(gym.Env):
 			self.last_target_distance = self.calc_target_distance()
 			self.last_velocity_distance = self.calc_velocity_distance()
 
+			# reset mass location
+			x  = self.np_random.uniform(low=-self.TARG_LIMIT, high=self.TARG_LIMIT)
+			y  = self.np_random.uniform(low=-self.TARG_LIMIT, high=self.TARG_LIMIT)
+			x_vel = self.np_random.uniform(low=-1, high=1)
+			y_vel = self.np_random.uniform(low=-1, high=1)
+
+			self.initalize_start_pos([x,y],[x_vel,y_vel])
+
 			lookat = [0,0,0.1]
 			distance = 7
 			yaw = 0
@@ -210,11 +230,11 @@ class pointMassEnv(gym.Env):
 # test_env.render(mode = 'human')
 # test_env.reset()
 
-# for i in range(0,10000):
+# for i in range(0,100000):
 
 # 	#env._p.stepSimulation()
 # 	time.sleep(0.005)
-# 	action = [test_env._p.readUserDebugParameter(test_env.x_shift), test_env._p.readUserDebugParameter(test_env.y_shift)]
+# 	action = np.array([test_env._p.readUserDebugParameter(test_env.x_shift), test_env._p.readUserDebugParameter(test_env.y_shift)])
 # 	o2, r, d, _ = test_env.step(action)
 # 	print(o2)
 
